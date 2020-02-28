1becad8567621a62cb5ac990eec620446929a965
Orlando Wilson <orlandowilson@Orlandos-MacBook-Pro.local>
2020-02-28 06:51:46 -0500

    adding angular.js for ServiceNow native tests

--- server/src/public/index.js
+++ server/src/public/index.js
@@ -1,20 +1,36 @@
-let requestBody = "";
+// let requestBody = "";
 
-let client = new XMLHttpRequest();
-client.open("get", "http://localhost:3000/table");
+// let client = new XMLHttpRequest();
+// client.open("get", "http://localhost:3000/table");
 
-client.onreadystatechange = function () {
-	if (this.readyState == this.DONE) {
-		document.getElementById("response").innerHTML = this.response;
-	}
-};
+// client.onreadystatechange = function () {
+// 	if (this.readyState == this.DONE) {
+// 		document.getElementById("response").innerHTML = this.response;
+// 	}
+// };
 
-const send = () => client.send(requestBody);
+// const send = () => client.send(requestBody);
 
-// angular.module('Snow',['ngRoute','ngResource'])
-// .controller('AppsOptsCtrl',function($scope,$http){
-//     $scope.title = 'ngServiceNow';
-//     $http.get('test').then(function(response){
-//         console.log(response);
-//     })
-// })
+angular.module('Snow',['ngRoute','ngResource'])
+.controller('AppsOptsCtrl',function($scope,$http){
+
+	$scope.title = 'ngServiceNow';
+	$scope.url = 'http://localhost:3000/table';
+	$scope.data = [];
+	$scope.errorMsg = '';
+	
+    $http.get($scope.url).then(function successCb(response){
+		
+		$scope.data = [];
+		$scope.errorMsg = 'No Error(s) Found';
+
+		console.log('ServiceNow Response: ', response);
+		if (response.status === 200 && response.data && response.data.result) {
+			console.log(response.status);
+			$scope.data = response.data.result
+		}
+    },function errorCb(response){
+		console.log('Error Response:' , response);
+		$scope.errorMsg = `Error Status: ${response.status}: ${response.data}`;
+	})
+})
