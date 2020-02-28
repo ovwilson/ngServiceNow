71ea2ace95bb3eb08feb3a11d6c08fe64292e1da
orlando <landough@Orlandos-iMac.local>
2020-02-28 11:16:47 -0500

    removed old xhr request comments

--- server/src/public/javascripts/index.js
+++ server/src/public/javascripts/index.js
@@ -1,36 +1,23 @@
-// let requestBody = "";
+angular.module('Snow', ['ngRoute', 'ngResource'])
+	.controller('AppsOptsCtrl', function ($scope, $http) {
 
-// let client = new XMLHttpRequest();
-// client.open("get", "http://localhost:3000/table");
-
-// client.onreadystatechange = function () {
-// 	if (this.readyState == this.DONE) {
-// 		document.getElementById("response").innerHTML = this.response;
-// 	}
-// };
-
-// const send = () => client.send(requestBody);
-
-angular.module('Snow',['ngRoute','ngResource'])
-.controller('AppsOptsCtrl',function($scope,$http){
-
-	$scope.title = 'ngServiceNow';
-	$scope.url = 'http://localhost:3000/table';
-	$scope.data = [];
-	$scope.errorMsg = '';
-	
-    $http.get($scope.url).then(function successCb(response){
-		
+		$scope.title = 'ngServiceNow';
+		$scope.url = 'http://localhost:3000/table';
 		$scope.data = [];
-		$scope.errorMsg = 'No Error(s) Found';
+		$scope.errorMsg = '';
 
-		console.log('ServiceNow Response: ', response);
-		if (response.status === 200 && response.data && response.data.result) {
-			console.log(response.status);
-			$scope.data = response.data.result
-		}
-    },function errorCb(response){
-		console.log('Error Response:' , response);
-		$scope.errorMsg = `Error Status: ${response.status}: ${response.data}`;
-	})
-})
+		$http.get($scope.url).then(function successCb(response) {
+
+			$scope.data = [];
+			$scope.errorMsg = 'No Error(s) Found';
+
+			console.log('ServiceNow Response: ', response);
+			if (response.status === 200 && response.data && response.data.result) {
+				console.log(response.status);
+				$scope.data = response.data.result;
+			}
+		}, function errorCb(response) {
+			console.log('Error Response:', response);
+			$scope.errorMsg = `Error Status: ${response.status}: ${response.data}`;
+		});
+	});
